a772169d651b2439102ca9c7199ce51974bf8fe0
madmax100 <cirilomax@gmail.com>
2015-02-22 11:39:44 -0300

    jogo2-v1

--- BasicExample.js
+++ BasicExample.js
@@ -1,9 +1,11 @@
 "use strict";
 
+var teclas={};
+
 var Game = {
     canvas : undefined,
     canvasContext : undefined,
-    rectanglePosition: 0
+    rectanglePosition : 0
 };
 
 Game.start = function () {
@@ -12,30 +14,42 @@ Game.start = function () {
     Game.mainLoop();
 };
 
-
-
-Game.update = function () {
-	var d = new Date();
-    Game.rectanglePosition = d.getTime() % Game.canvas.width;
-
-};
+document.addEventListener( 'DOMContentLoaded', Game.start);
+document.addEventListener("keydown" , function(e){
+				teclas[e.keyCode] = true;
+				//alert(e.keyCode);
+			} , false );
+document.addEventListener("keyup" , function(e){
+				delete teclas[e.keyCode];
+			} , false );
 
 Game.clearCanvas = function () {
     Game.canvasContext.clearRect(0, 0, Game.canvas.width, Game.canvas.height);
 };
 
+Game.mainLoop = function() {
+	Game.clearCanvas();
+    Game.update();
+    Game.draw();
+    window.setTimeout(Game.mainLoop, 1000 / 60);
+};
+
+Game.update = function () {
+	//var d = new Date();
+   //Game.rectanglePosition = d.getTime() % Game.canvas.width;
+   Game.movequadrado();
+};
+
 
 Game.draw = function () {
     Game.canvasContext.fillStyle = "blue";
-    Game.canvasContext.fillRect(0, 0, Game.canvas.width, Game.canvas.height);
     Game.canvasContext.fillRect(Game.rectanglePosition, 100, 50, 50);
 };
 
-Game.mainLoop = function () {
-	//Game.clearCanvas();
-    //Game.update();
-    Game.draw();
-    window.setTimeout(mainLoop, 1000 / 60);
-};
+Game.movequadrado = function(){
+	if(39 in teclas) {
+		Game.rectanglePosition = Game.rectanglePosition+10; 			
+	}
 
-document.addEventListener('DOMContentLoaded', Game.start);
+}
+
